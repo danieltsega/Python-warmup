c7754e1b4034402362f7159f023bb1a08482b4df
DannySanchez6658 <danieltsega6658@gmail.com>
2024-03-14 19:35:19 +0300

    [Exercise]: Add exercise 1

---
+++ Data Types/exercise1.py
@@ -0,0 +1,10 @@
+# Create a program that splits 2-digit numbers and add them
+
+two_digit_num = "48"
+
+digit_one = int(two_digit_num[0])
+digit_two = int(two_digit_num[1])
+
+sum = digit_one + digit_two
+
+print(sum)
